912906b56bad50e21ca6f5f32a036e6ad6b18ed8
MeTubeapp <62765738+MeTubeapp@users.noreply.github.com>
2021-04-18 16:37:32 +0530

    Update raw_youtube_player.dart

--- packages/youtube_player_flutter/lib/src/player/raw_youtube_player.dart
+++ packages/youtube_player_flutter/lib/src/player/raw_youtube_player.dart
@@ -76,10 +76,8 @@ class _RawYoutubePlayerState extends State<RawYoutubePlayer>
       child: InAppWebView(
         key: widget.key,
         initialData: InAppWebViewInitialData(
-          data: player,
-          baseUrl: Uri.parse('https://www.youtube.com'),
-          encoding: 'utf-8',
-          mimeType: 'text/html',
+          data: controller!.initialVideoId,
+          
         ),
         initialOptions: InAppWebViewGroupOptions(
           crossPlatform: InAppWebViewOptions(
@@ -236,157 +234,157 @@ class _RawYoutubePlayerState extends State<RawYoutubePlayer>
     );
   }
 
-  String get player => '''
-    <!DOCTYPE html>
-    <html>
-    <head>
-        <style>
-            html,
-            body {
-                margin: 0;
-                padding: 0;
-                background-color: #000000;
-                overflow: hidden;
-                position: fixed;
-                height: 100%;
-                width: 100%;
-                pointer-events: none;
-            }
-        </style>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'>
-    </head>
-    <body>
-        <div id="player"></div>
-        <script>
-            var tag = document.createElement('script');
-            tag.src = "https://www.youtube.com/iframe_api";
-            var firstScriptTag = document.getElementsByTagName('script')[0];
-            firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-            var player;
-            var timerId;
-            function onYouTubeIframeAPIReady() {
-                player = new YT.Player('player', {
-                    height: '100%',
-                    width: '100%',
-                    videoId: '${controller!.initialVideoId}',
-                    playerVars: {
-                        'controls': 0,
-                        'playsinline': 1,
-                        'enablejsapi': 1,
-                        'fs': 0,
-                        'rel': 0,
-                        'showinfo': 0,
-                        'iv_load_policy': 3,
-                        'modestbranding': 1,
-                        'cc_load_policy': ${boolean(value: controller!.flags.enableCaption)},
-                        'cc_lang_pref': '${controller!.flags.captionLanguage}',
-                        'autoplay': ${boolean(value: controller!.flags.autoPlay)},
-                        'start': ${controller!.flags.startAt},
-                        'end': ${controller!.flags.endAt}
-                    },
-                    events: {
-                        onReady: function(event) { window.flutter_inappwebview.callHandler('Ready'); },
-                        onStateChange: function(event) { sendPlayerStateChange(event.data); },
-                        onPlaybackQualityChange: function(event) { window.flutter_inappwebview.callHandler('PlaybackQualityChange', event.data); },
-                        onPlaybackRateChange: function(event) { window.flutter_inappwebview.callHandler('PlaybackRateChange', event.data); },
-                        onError: function(error) { window.flutter_inappwebview.callHandler('Errors', error.data); }
-                    },
-                });
-            }
+//   String get player => '''
+//     <!DOCTYPE html>
+//     <html>
+//     <head>
+//         <style>
+//             html,
+//             body {
+//                 margin: 0;
+//                 padding: 0;
+//                 background-color: #000000;
+//                 overflow: hidden;
+//                 position: fixed;
+//                 height: 100%;
+//                 width: 100%;
+//                 pointer-events: none;
+//             }
+//         </style>
+//         <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'>
+//     </head>
+//     <body>
+//         <div id="player"></div>
+//         <script>
+//             var tag = document.createElement('script');
+//             tag.src = "https://www.youtube.com/iframe_api";
+//             var firstScriptTag = document.getElementsByTagName('script')[0];
+//             firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+//             var player;
+//             var timerId;
+//             function onYouTubeIframeAPIReady() {
+//                 player = new YT.Player('player', {
+//                     height: '100%',
+//                     width: '100%',
+//                     videoId: '${controller!.initialVideoId}',
+//                     playerVars: {
+//                         'controls': 0,
+//                         'playsinline': 1,
+//                         'enablejsapi': 1,
+//                         'fs': 0,
+//                         'rel': 0,
+//                         'showinfo': 0,
+//                         'iv_load_policy': 3,
+//                         'modestbranding': 1,
+//                         'cc_load_policy': ${boolean(value: controller!.flags.enableCaption)},
+//                         'cc_lang_pref': '${controller!.flags.captionLanguage}',
+//                         'autoplay': ${boolean(value: controller!.flags.autoPlay)},
+//                         'start': ${controller!.flags.startAt},
+//                         'end': ${controller!.flags.endAt}
+//                     },
+//                     events: {
+//                         onReady: function(event) { window.flutter_inappwebview.callHandler('Ready'); },
+//                         onStateChange: function(event) { sendPlayerStateChange(event.data); },
+//                         onPlaybackQualityChange: function(event) { window.flutter_inappwebview.callHandler('PlaybackQualityChange', event.data); },
+//                         onPlaybackRateChange: function(event) { window.flutter_inappwebview.callHandler('PlaybackRateChange', event.data); },
+//                         onError: function(error) { window.flutter_inappwebview.callHandler('Errors', error.data); }
+//                     },
+//                 });
+//             }
 
-            function sendPlayerStateChange(playerState) {
-                clearTimeout(timerId);
-                window.flutter_inappwebview.callHandler('StateChange', playerState);
-                if (playerState == 1) {
-                    startSendCurrentTimeInterval();
-                    sendVideoData(player);
-                }
-            }
+//             function sendPlayerStateChange(playerState) {
+//                 clearTimeout(timerId);
+//                 window.flutter_inappwebview.callHandler('StateChange', playerState);
+//                 if (playerState == 1) {
+//                     startSendCurrentTimeInterval();
+//                     sendVideoData(player);
+//                 }
+//             }
 
-            function sendVideoData(player) {
-                var videoData = {
-                    'duration': player.getDuration(),
-                    'title': player.getVideoData().title,
-                    'author': player.getVideoData().author,
-                    'videoId': player.getVideoData().video_id
-                };
-                window.flutter_inappwebview.callHandler('VideoData', videoData);
-            }
+//             function sendVideoData(player) {
+//                 var videoData = {
+//                     'duration': player.getDuration(),
+//                     'title': player.getVideoData().title,
+//                     'author': player.getVideoData().author,
+//                     'videoId': player.getVideoData().video_id
+//                 };
+//                 window.flutter_inappwebview.callHandler('VideoData', videoData);
+//             }
 
-            function startSendCurrentTimeInterval() {
-                timerId = setInterval(function () {
-                    window.flutter_inappwebview.callHandler('VideoTime', player.getCurrentTime(), player.getVideoLoadedFraction());
-                }, 100);
-            }
+//             function startSendCurrentTimeInterval() {
+//                 timerId = setInterval(function () {
+//                     window.flutter_inappwebview.callHandler('VideoTime', player.getCurrentTime(), player.getVideoLoadedFraction());
+//                 }, 100);
+//             }
 
-            function play() {
-                player.playVideo();
-                return '';
-            }
+//             function play() {
+//                 player.playVideo();
+//                 return '';
+//             }
 
-            function pause() {
-                player.pauseVideo();
-                return '';
-            }
+//             function pause() {
+//                 player.pauseVideo();
+//                 return '';
+//             }
 
-            function loadById(loadSettings) {
-                player.loadVideoById(loadSettings);
-                return '';
-            }
+//             function loadById(loadSettings) {
+//                 player.loadVideoById(loadSettings);
+//                 return '';
+//             }
 
-            function cueById(cueSettings) {
-                player.cueVideoById(cueSettings);
-                return '';
-            }
+//             function cueById(cueSettings) {
+//                 player.cueVideoById(cueSettings);
+//                 return '';
+//             }
 
-            function loadPlaylist(playlist, index, startAt) {
-                player.loadPlaylist(playlist, 'playlist', index, startAt);
-                return '';
-            }
+//             function loadPlaylist(playlist, index, startAt) {
+//                 player.loadPlaylist(playlist, 'playlist', index, startAt);
+//                 return '';
+//             }
 
-            function cuePlaylist(playlist, index, startAt) {
-                player.cuePlaylist(playlist, 'playlist', index, startAt);
-                return '';
-            }
+//             function cuePlaylist(playlist, index, startAt) {
+//                 player.cuePlaylist(playlist, 'playlist', index, startAt);
+//                 return '';
+//             }
 
-            function mute() {
-                player.mute();
-                return '';
-            }
+//             function mute() {
+//                 player.mute();
+//                 return '';
+//             }
 
-            function unMute() {
-                player.unMute();
-                return '';
-            }
+//             function unMute() {
+//                 player.unMute();
+//                 return '';
+//             }
 
-            function setVolume(volume) {
-                player.setVolume(volume);
-                return '';
-            }
+//             function setVolume(volume) {
+//                 player.setVolume(volume);
+//                 return '';
+//             }
 
-            function seekTo(position, seekAhead) {
-                player.seekTo(position, seekAhead);
-                return '';
-            }
+//             function seekTo(position, seekAhead) {
+//                 player.seekTo(position, seekAhead);
+//                 return '';
+//             }
 
-            function setSize(width, height) {
-                player.setSize(width, height);
-                return '';
-            }
+//             function setSize(width, height) {
+//                 player.setSize(width, height);
+//                 return '';
+//             }
 
-            function setPlaybackRate(rate) {
-                player.setPlaybackRate(rate);
-                return '';
-            }
+//             function setPlaybackRate(rate) {
+//                 player.setPlaybackRate(rate);
+//                 return '';
+//             }
 
-            function setTopMargin(margin) {
-                document.getElementById("player").style.marginTop = margin;
-                return '';
-            }
-        </script>
-    </body>
-    </html>
-  ''';
+//             function setTopMargin(margin) {
+//                 document.getElementById("player").style.marginTop = margin;
+//                 return '';
+//             }
+//         </script>
+//     </body>
+//     </html>
+//   ''';
 
   String boolean({required bool value}) => value == true ? "'1'" : "'0'";
 
